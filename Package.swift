1dbeb3a08e85b38a5b33f6f1de5092bfddd111a4
Kevin Heredia <kevin@ravecodesolutions.com>
2024-11-20 11:26:50 -0500

    update keychain

--- Package.swift
+++ Package.swift
@@ -11,11 +11,19 @@ let package = Package(
             name: "KcLibrarySwift",
             targets: ["KcLibrarySwift"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/evgenyneu/keychain-swift.git", .upToNextMajor(from: "20.0.0"))
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "KcLibrarySwift"),
+            name: "KcLibrarySwift",
+            dependencies: [
+                .product(name: "KeychainSwift", package: "keychain-swift"),
+            ]
+        ),
+        
 
     ]
 )
